d088daa069fe1f30232691b5ba50c78f5a927c3c
Vamston Yang <vamston@gmail.com>
2014-10-17 15:54:02 +0800

    File structure changed

---
+++ Reddit/Reddit/InitialViewController.swift
@@ -0,0 +1,35 @@
+//
+//  InitialViewController.swift
+//  Reddit
+//
+//  Created by Vamston Yang on 10/17/14.
+//  Copyright (c) 2014 Vamston Yang. All rights reserved.
+//
+
+import UIKit
+
+class InitialViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
